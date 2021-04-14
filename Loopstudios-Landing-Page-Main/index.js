0ad0b7f37c77357c6489185bb222e3bc0e747697
Bobi <borislav.d.nastev@gmail.com>
2021-04-14 15:11:42 +0300

    Revert "Added Loopstudios-Landing-Page-Main"
    This reverts commit 5a93e4650391c35e5fdf2a430ae385abdac01048.

--- Loopstudios-Landing-Page-Main/index.js
+++
@@ -1,9 +0,0 @@
-const burgerBtn = document.querySelector('.burger');
-const navMenu = document.querySelector('.nav_items');
-
-burgerBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-  burgerBtn.classList.toggle('active');
-  navMenu.classList.toggle('active');
-}
